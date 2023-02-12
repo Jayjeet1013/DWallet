473d859e471ffa8e984c8620f8181c3b28348d9a
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-12 22:20:02 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-
+import SimpleStorage from './contracts/SimpleStorage.json';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+  async function template() {
+    const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+    const web3 = new Web3(provider);
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = SimpleStorage.networks[networkId];
+    console.log(deployedNetwork.address);
+  }
+  template();
+
   return <div className="App">
       
     </div>;
