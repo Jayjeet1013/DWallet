8bb82e9f5107d8cf4e149656792ef5e0190edab5
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-13 19:53:05 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import { useState} from 'react'
 import SimpleStorage from './contracts/SimpleStorage.json';
 import Web3 from 'web3';
 import './App.css';
 
 function App() {
+  const [state,setState] = useState({
+    web3:null,
+    contract:null
+  })
   async function template() {
     const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
     const web3 = new Web3(provider);
@@ -13,8 +18,10 @@ function App() {
       SimpleStorage.abi,
       deployedNetwork.address
     );
-    console.log(contract);
+    setState({web3:web3,contract:contract})
+    // console.log(contract);
   }
+  console.log(state);
   template();
 
   return <div className="App">
